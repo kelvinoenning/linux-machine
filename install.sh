7c849b2bc7b69f4fa72b018f36be43426b815fa1
Kelvin Oenning <kelvinstang@hotmail.com>
2020-06-02 10:06:14 -0300

    Initial commit

---
+++ install.sh
@@ -0,0 +1,49 @@
+#!/bin/sh
+
+printStage () {
+   echo "\n\n====================\n"$1"\n====================\n\n"
+}
+
+update() {
+   apt update
+}
+
+upgrade() {
+   apt upgrade -y
+}
+
+printStage "Update repositories"
+
+update
+
+printStage "Upgrade"
+
+upgrade
+
+printStage "Install >> curl"
+
+apt install apt-transport-https curl
+
+printStage "Install >> git"
+
+apt install git
+
+printStage "Install >> nvim"
+
+rm /usr/bin/nvim
+curl -LO https://github.com/neovim/neovim/releases/download/stable/nvim.appimage
+mv nvim.appimage /usr/bin/nvim
+chmod +x /usr/bin/nvim
+curl -fLo ~/.config/nvim/autoload/plug.vim --create-dirs \
+       https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+
+printStage "Install >> Silver Searcher"
+
+sudo apt-get install silversearcher-ag
+
+printStage "Install >> Brave browser"
+
+curl -s https://brave-browser-apt-release.s3.brave.com/brave-core.asc | sudo apt-key --keyring /etc/apt/trusted.gpg.d/brave-browser-release.gpg add -
+echo "deb [arch=amd64] https://brave-browser-apt-release.s3.brave.com/ stable main" | sudo tee /etc/apt/sources.list.d/brave-browser-release.list
+update
+apt install brave-browser
