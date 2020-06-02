03bae41eb4b7d92e8de2d1018645e52bd8b5ea63
Kelvin Oenning <kelvinstang@hotmail.com>
2020-06-02 17:01:25 -0300

    Fixes

--- install.sh
+++ install.sh
@@ -31,16 +31,17 @@ apt install git -y
 printStage "Install >> nvim"
 
 rm /usr/bin/nvim
-curl -LO https://github.com/neovim/neovim/releases/download/stable/nvim.appimage
-mv nvim.appimage /usr/bin/nvim
+curl -O /tmp/nvim.appimage https://github.com/neovim/neovim/releases/download/stable/nvim.appimage
+mv /tmp/nvim.appimage /usr/bin/nvim
 chmod +x /usr/bin/nvim
-mkdir -p ~/.config/nvim && cp ./config/nvim/init.vim ~/.config/nvim/init.vim
+mkdir -p ~/.config/nvim
+cp ./config/nvim/init.vim ~/.config/nvim/init.vim
 curl -fLo ~/.config/nvim/autoload/plug.vim --create-dirs \
        https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
 printStage "Install >> Silver Searcher"
 
-sudo apt-get install silversearcher-ag -y
+apt install silversearcher-ag -y
 
 printStage "Install >> Brave browser"
 
@@ -51,5 +52,5 @@ apt install brave-browser -y
 
 printStage "Install >> Zoom"
 
-wget -O ~/tmp/zoom.deb https://zoom.us/client/latest/zoom_amd64.deb
-apt install ~/tmp/zoom.deb
+wget -O /tmp/zoom.deb https://zoom.us/client/latest/zoom_amd64.deb
+apt install /tmp/zoom.deb -y
