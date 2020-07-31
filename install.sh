567283ad8d200e992540908f4ad05284f3cab1c5
Kelvin Oenning <kelvinstang@hotmail.com>
2020-07-30 22:16:29 -0300

    Adds golang 1.14.6

--- install.sh
+++ install.sh
@@ -33,6 +33,29 @@ printStage "Install >> git"
 
 apt install git -y
 
+printStage "Install >> asdf"
+
+apt-get install dirmngr -y
+apt-get install gpg -y
+
+git clone https://github.com/asdf-vm/asdf.git ~/.asdf --branch v0.7.8
+echo -e '\n. $HOME/.asdf/asdf.sh' >> ~/.bashrc
+echo -e '\n. $HOME/.asdf/completions/asdf.bash' >> ~/.bashrc
+source ~/.bashrc
+
+printStage "Install >> asdf >> nodejs"
+
+asdf plugin-add nodejs https://github.com/asdf-vm/asdf-nodejs.git
+bash ~/.asdf/plugins/nodejs/bin/import-release-team-keyring
+asdf install nodejs 14.4.0
+asdf global nodejs 14.4.0
+
+printStage "Install >> asdf >> golang"
+
+asdf plugin-add golang https://github.com/kennyp/asdf-golang.git
+asdf install golang 1.14.6
+asdf global golang 1.14.6
+
 printStage "Install >> nvim"
 
 rm /usr/bin/nvim
